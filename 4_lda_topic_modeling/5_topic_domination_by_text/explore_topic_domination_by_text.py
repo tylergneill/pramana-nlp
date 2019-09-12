8ddaad7a98dabc6ac794f6c4627f418d38e61f08
Tyler Neill <tyler.g.neill@gmail.com>
2019-09-12 18:05:41 -0400

    add 1_text_original, rest of 4_lda_topic_modeling

---
+++ 4_lda_topic_modeling/5_topic_domination_by_text/explore_topic_domination_by_text.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+## considers top n documents for each of k topics, parses identifiers to reveal which texts dominate that topic
+
+import requests
+import re
+from collections import Counter
+from tqdm import tqdm
+
+n = 1000
+k = 50
+url = "http://localhost:3737/topic/%s/%s"
+topic_search_terms = [str(x+1) for x in range(k)]
+doc_search_depth = str(n)
+
+regex_replacements = [
+[u'^[a-zāīūṛṝḷṅñṭḍṇśṣḥṃ].*$',''],
+[r'^[RT].*$',''],
+[r'_.*$',''],
+[r'\n{2,}','\n'],
+[r'\A\s*', ''],
+[r'\s*\Z', '']
+]
+
+out_buff = ""
+for topic_search_term in tqdm(topic_search_terms):
+	r = requests.get(url % (topic_search_term, doc_search_depth) )
+	text = r.text
+	for r_r in regex_replacements: text = re.sub(r_r[0], r_r[1], text, flags=re.MULTILINE)
+	ids = text.split('\n')
+	counts = {}
+	for key, value in Counter(ids).items(): counts[key] = value
+	sorted_counts = sorted(counts.items(), key=lambda x: -x[1])
+	out_buff = out_buff + "Topic: %s\n" % topic_search_term
+	for i in sorted_counts: out_buff = out_buff + i[0] + ' ' + str(i[1]) + '\n'
+	out_buff = out_buff + '\n'
+f_out = open('topics_by_text_%s_%s.txt' % (str(n), str(k)),'w')
+f_out.write(out_buff.encode('utf-8'))
+f_out.close()
---
+++ 4_lda_topic_modeling/8_doc_similarity_table/format_doc_similarity_table.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+## considers closest n documents to query document, prioritizes set of preferred texts, formats 
+## relies on modified metallo.go
+
+query_id = "NBhū_104,6^1"
+n = 50
+priority_ranking = [u'PVin', u'PVA', u'NBh', u'NBhū', u'NV', u'PDhS']
+
+import requests
+import json
+from collections import OrderedDict
+
+class Hit():
+	def __init__(self):
+		self.id_name = ""
+		self.id_location = ""
+		self.rank = 0
+		self.distance = ""
+#		self.topics = []
+#		self.text = ""
+
+url = "http://localhost:3737/view/%s/%d/json" % (query_id, n)
+response = requests.get(url)
+json_data = json.loads(response.text)
+hits = json_data['items']
+real_hits = hits[1:]
+
+hits_by_text = OrderedDict()
+
+for h in real_hits:
+	H = Hit()
+	H.id_name = h['id'][:h['id'].find('_')]
+	H.id_location = h['id'][h['id'].find('_')+1:]
+	H.rank = h['rank']
+	H.distance = h['distance']
+
+	if H.id_name not in hits_by_text:
+		hits_by_text[H.id_name] = [(H.rank, H.id_location)]
+	elif H.id_name in hits_by_text:
+		hits_by_text[H.id_name].append( (H.rank, H.id_location) )
+
+prioritized_hits = OrderedDict()
+
+text_rank = {}
+for id in priority_ranking:
+	if id not in hits_by_text.keys(): continue
+	prioritized_hits[id] = hits_by_text[id]
+	hits_by_text.pop(id)
+	text_rank[id] = len(prioritized_hits)
+for k in hits_by_text.keys():
+	prioritized_hits[k] = hits_by_text[k]
+	hits_by_text.pop(k)
+	text_rank[k] = len(prioritized_hits)
+
+output_lines = []
+output_lines.append( [ '\t' + '\t'.join( prioritized_hits.keys() ) ] )
+for i in range( len(real_hits) ):
+ 	output_lines.append( [ '\t' for j in range( len(text_rank)+1 ) ] )
+ 	output_lines[i+1][0] = str(i+1) + '\t'
+
+for j, (id_name, hits) in enumerate(prioritized_hits.items()):
+	for rank, id_location in hits:
+		output_lines[rank][j+1] = id_location
+
+f_out = open('doc_similarity_table_%s_%d.tsv' % (query_id, n), 'w')
+f_out.write( '\n'.join( [''.join(o_l) for o_l in output_lines] ).encode('utf-8') )
+f_out.close()
