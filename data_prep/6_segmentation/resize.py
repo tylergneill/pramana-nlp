07c97986f845589bb8e3b22d81ccf831aa66aa59
tylergneill <tyler.g.neill@gmail.com>
2021-02-21 15:43:11 -0500

    restore and lightly update resizing

--- data_prep/6_segmentation/resize.py
+++ data_prep/6_segmentation/resize.py
@@ -1,7 +1,8 @@
 import re
 
-a = 300; b = 1000 # document character length parameters
-c = 0.8 # document split location (= percentage range from middle) parameter
+min_doc_size = 300 # char len
+max_doc_size = 1000 # char len
+crp = 0.8 # center range parameter for split (= possible % range from center)
 
 doc_splitpoint_regexes = [
 	u'[\.\?\|/]',
@@ -11,20 +12,24 @@ doc_splitpoint_regexes = [
 	u'[ ]',
 ]
 
-def resize(doc_L, identifer_L):
-	# results in min_doc_size a
-	# results in max_doc_size mostly b and in a very few cases a+b
-	doc_L, identifer_L = split_big_docs(doc_L, identifer_L)
-	return combine_small_docs(doc_L, identifer_L)
+# NOT USING ANYMORE
+# def resize(doc_L, identifer_L):
+# 	doc_L, identifer_L = split_big_docs(doc_L, identifer_L)
+# 	return combine_small_docs(doc_L, identifer_L)
 
 def no_small_docs(doc_L):
 	for l in [len(x) for x in doc_L]:
-		if l < a: return False
-	else: return True
+		if l < min_doc_size:
+			return False
+	else:
+		return True
+
 def no_big_docs(doc_L):
 	for l in [len(x) for x in doc_L]:
-		if l > b: return False
-	else: return True
+		if l > max_doc_size:
+			return False
+	else:
+		return True
 
 """
 Splitting functions (4):
@@ -34,6 +39,7 @@ Splitting functions (4):
 	find_midpoint()
 """
 
+# VERY OLD
 # def split_big_docs(doc_L, identifer_L):
 # 	"""
 # 	Guarantees successful split through cascade algorithm.
@@ -59,18 +65,21 @@ Splitting functions (4):
 #
 # 	print "ERROR, ALL SPLIT ATTEMPTS FAILED"; exit()
 
+
 def split_big_docs(doc_L, identifer_L):
 	"""
-	max_len will be between 1.0 * b and 1.2 * b
+	max_len will be between 1.0 * max_doc_size and 1.2 * max_doc_size
+	# THEN IT'S NOT 'MAX' NOW IS IT?? FIX!
 	"""
 
 	if no_big_docs(doc_L): return doc_L, identifer_L
 
+	# import pdb; pdb.set_trace()
 	doc_L2 = []
 	identifer_L2 = []
 	for (doc, identifer) in zip(doc_L, identifer_L):
 
-		if len(doc) <= b:
+		if len(doc) <= max_doc_size:
 			doc_L2.append(doc) # save appropriately sized doc
 			identifer_L2.append(identifer) # also append current identifer_L
 
@@ -85,12 +94,12 @@ def split_big_docs(doc_L, identifer_L):
 				doc_L2.append(new_doc)
 
 				new_identifer = identifer[:-1].replace('_',u'–') + '^%d]' % (i+1)
-				logging.debug( identifer.ljust(35) + '(split)'.ljust(35) + new_identifer )
+				# logging.debug( identifer.ljust(35) + '(split)'.ljust(35) + new_identifer )
 				identifer_L2.append(new_identifer)
 
-	return doc_L2, identifer_L2 # if len(doc_L2) == 1, that means split failed
+	return doc_L2, identifer_L2 # if len(doc_L2) == 1, means that split failed
 
-def split_unit(unit, splitpoint_regexes = doc_splitpoint_regexes, max_len = b):
+def split_unit(unit, splitpoint_regexes = doc_splitpoint_regexes, max_len = max_doc_size):
 	"""
 	Recursively split unit into smaller documents according to splitpoint_regex until all resulting docs conform to max_len.
 	unit may be a doc (when adjusting docs) or a line (when preparing for segmenter).
@@ -104,7 +113,7 @@ def split_unit(unit, splitpoint_regexes = doc_splitpoint_regexes, max_len = b):
 	else:
 		for splitpoint_regex in splitpoint_regexes:
 			midpoint = find_midpoint(unit, splitpoint_regex)
-			if midpoint > (1.0 - (1.0 - c) / 2) * len(unit) or midpoint < ((1.0 - c) / 2) * len(unit): continue
+			if midpoint > (1.0 - (1.0 - crp) / 2) * len(unit) or midpoint < ((1.0 - crp) / 2) * len(unit): continue
 			else: break
 
 		part_a = split_unit( unit[ : midpoint + 1] , splitpoint_regexes, max_len)
@@ -141,7 +150,7 @@ def combine_small_docs(doc_L, identifer_L):
 
 	for i, (doc, identifer) in enumerate(zip(doc_L, identifer_L)):
 
-		if len(doc_L[i]) >= a:
+		if len(doc_L[i]) >= min_doc_size:
 			if doc_L2[-1] == '':
 				doc_L2[-1] = doc_L[i]
 				identifer_L2[-1] = identifer_L[i]
@@ -236,32 +245,32 @@ def combine_two_identifers(identifer_A, identifer_B):
 
 	identifier_AB = "[%s_%s]" % (first_part_of_A, last_part_of_B)
 
-	log_id_combo(identifer_A, identifer_B, identifier_AB)
+	# log_id_combo(identifer_A, identifer_B, identifier_AB)
 	return identifier_AB
 
-import logging
-logging.basicConfig(filename='identifier_log.txt',level=logging.DEBUG)
+# import logging
+# logging.basicConfig(filename='identifier_log.txt',level=logging.DEBUG)
+#
+# def log_id_combo(A, B, AB):
+# 	logging.debug( A.ljust(35) + B.ljust(35) + AB )
 
-def log_id_combo(A, B, AB):
-	logging.debug( A.ljust(35) + B.ljust(35) + AB )
-
-
-"""
-Initialization function (1)
-"""
-
-def initialize_doc_identifers(section_identifer, doc_identifers):
-	"""
-	If doc identifier empty, number with numerical parent section identifer plus integer increment.
-	"""
-
-	for i, doc_identifer in enumerate(doc_identifers):
-
-		old_identifier = doc_identifer # for logging
-
-		if doc_identifers[i] == ('[]'):
-			doc_identifers[i] = '[' + section_identifer[1:-1] + '.%d]' % (i+1)
-
-		logging.debug( old_identifier.ljust(35) + '(init)'.ljust(35) + doc_identifers[i] )
-
-	return doc_identifers
+#
+# """
+# Initialization function (1)
+# """
+#
+# def initialize_doc_identifers(section_identifer, doc_identifers):
+# 	"""
+# 	If doc identifier empty, number with numerical parent section identifer plus integer increment.
+# 	"""
+#
+# 	for i, doc_identifer in enumerate(doc_identifers):
+#
+# 		old_identifier = doc_identifer # for logging
+#
+# 		if doc_identifers[i] == ('[]'):
+# 			doc_identifers[i] = '[' + section_identifer[1:-1] + '.%d]' % (i+1)
+#
+# 		logging.debug( old_identifier.ljust(35) + '(init)'.ljust(35) + doc_identifers[i] )
+#
+# 	return doc_identifers
