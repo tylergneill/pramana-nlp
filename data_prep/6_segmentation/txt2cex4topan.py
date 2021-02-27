f4e5d41a644d446166bd05c12c669fbb2155c0ee
tylergneill <tyler.g.neill@gmail.com>
2021-02-27 17:21:12 -0500

    clean out obsolete material and plan add of validation and cex output

--- data_prep/6_segmentation/txt2cex4topan.py
+++ data_prep/6_segmentation/txt2cex4topan.py
@@ -52,7 +52,6 @@ import subprocess
 from skrutable.splitter.wrapper import Splitter
 
 import resize
-# import resandhify
 # import validate
 
 input_path = 'data/input/'
@@ -62,6 +61,9 @@ logging_path_prefix = 'data/output/logging/'
 stats_buffer = '\t'.join([
 	"filename", "docs_orig", "docs_resized", "seg_lines", "tokens"
 	]) + '\n' # header row
+
+# initialize CTS file buffer here...
+
 
 class Section(object):
 
@@ -73,11 +75,6 @@ class Section(object):
 		self.doc_count_after_resize = 0
 		self.word_split_line_count = 0
 		self.token_count = 0
-
-#	# NOT DOING THIS ANYMORE
-# 	def initialize_doc_identifers(self):
-# 		self.doc_identifers = edit.initialize_doc_identifers(
-# 			self.section_label, self.doc_identifers)
 
 	def preclean(self):
 		for i in range(len(self.doc_contents)):
@@ -115,9 +112,11 @@ for fn in fns:
 	with open(full_input_path, 'r') as f_in:
 		f_data = f_in.read()
 
-	print("precleaning and parsing %s..." % fn)
+	print("validating and parsing %s..." % fn)
 
-	# structural (=bracket) validation at this point would be smart
+	# do structural validation here...
+
+	# could also do content validation here...
 
 	# do some structural cleaning
 	regex_replacement = [
@@ -132,7 +131,7 @@ for fn in fns:
 	for r_r in regex_replacement:
 		f_data = re.sub(r_r[0], r_r[1], f_data)
 		if r_r[0] in to_repeat: # repeat as necessary
-			while bool( re.match(r_r[0], f_data) ) == True:
+			while bool( re.search(r_r[0], f_data) ) == True:
 				f_data = re.sub(r_r[0], r_r[1], f_data)
 
 	file_sections = f_data.split('\n') # divide into {...} sections
@@ -160,7 +159,7 @@ for fn in fns:
 			all_doc_contents.append(doc_content)
 
 	# log
-	logging_path_suffix = '0_original_doc_candidates/'
+	logging_path_suffix = '1_original_doc_candidates/'
 	full_logging_f_path = os.path.join(logging_path_prefix, logging_path_suffix, fn)
 	with open(full_logging_f_path ,'w') as f_out:
 		f_out.write('\n'.join(
@@ -168,9 +167,7 @@ for fn in fns:
 			)
 		)
 
-	print("resizing %s..." % fn)
-
-	# now resize doc_contents, first restricted to sections
+	print("precleaning and resizing %s..." % fn)
 
 	megaS = Section() # will collect all adjusted documents and their identifiers
 	megaS.section_label = fn
@@ -188,118 +185,33 @@ for fn in fns:
 	megaS.doc_count_after_resize = len(megaS.doc_contents)
 
 	# log
-	logging_path_suffix = '1_resized_docs/'
+	logging_path_suffix = '2_resized_docs/'
 	full_logging_f_path = os.path.join(logging_path_prefix, logging_path_suffix, fn)
 	with open(full_logging_f_path ,'w') as f_out:
 		f_out.write('\n'.join(
 			[ id + '\t' + c for id, c in zip(megaS.doc_identifers, megaS.doc_contents) ]
 			)
 		)
-
-#	# NOT DOING THIS ANYMORE
-# 	# do one final resize over all docs
-#  	megaS.resize_docs()
-# 	# of debatable value, cp. e.g. VS 4,2 and 5,1 combined with each other based on size, despite 4,1 and 5,2
-
-	# now clean up and segment all doc_contents at once
-	# '#' marks doc boundaries to restore afterward
 
 	print("word-splitting %s..." % fn)
 
 	# presegmentation_content = '\n#\n'.join(megaS.doc_contents)
 	presegmentation_content = '\n'.join(megaS.doc_contents)
 
-	# log
-	logging_path_suffix = '2_pre-split_content/'
-	full_logging_f_path = os.path.join(logging_path_prefix, logging_path_suffix, fn)
-	with open(full_logging_f_path ,'w') as f_out:
-		f_out.write(presegmentation_content)
-
-#	# NOT DOING THIS ANYMORE
-# 	if fn in resandhify.to_do_list:
-# 		print('resandhifying....')
-# 		presegmentation_contents = resandhify.resandhify(presegmentation_contents)
-
 	# any remaining '-' must have been intentionally placed by me for segmentation
 	presegmentation_content = presegmentation_content.replace('-', ' ')
 
-# 	# use punctuation for more newlines
-# 	regex_replace = [
-# 	[u'([\|/\.,;:—\?] )', '\\1\n'],
-# 	# but not too many
-# 	[u'\n{2,}', '\n'],
-# 	]
-#
-# 	for r_r in regex_replace:
-# 		presegmentation_contents = re.sub(r_r[0], r_r[1], presegmentation_contents)
-#
-# 	# add further newlines to break up lines longer than 128 chars
-#
-# 	lines2 = []
-# 	for line in lines:
-# 		# all cases >128 must have already been handled manually during cleaning ('_')
-# 		# thus guarantees that split_unit() will work on first try
-#
-# 		try:
-# 			split_up_pieces = edit.split_unit(line, splitpoint_regexes = [u"[' ']"], max_len = 128)
-# 		except RuntimeError:
-# 			import pdb;pdb.set_trace()
-# 			split_up_pieces = edit.split_unit(line, splitpoint_regexes = [u"[' ']"], max_len = 128)
-# 		for l in split_up_pieces:
-# 			lines2.append(l)
-#
-#	print("number of lines for segmentation:", len(lines2))
-#
-# 	presegmentation_contents = '\n'.join(lines2)
-
 	postsegmentation_content = Spl.split(presegmentation_content, prsrv_punc=False)
-
-# 	# now, in order to do e.g. "from sanskrit import apply", would have to convert everything to python3
-# 	# instead, communicate with modified python3 'apply' file via external file i/o and subprocess
-# 	f_out = open('temp/temp2_resized_for_segmenter.txt','w')
-# 	f_out.write(presegmentation_contents.encode('utf-8'))
-# 	f_out.close()
-#
-# 	# raw_input("WAITING FOR HOOMAN") # here is where one can observe, by removing quotation '...', how important apostrophe is for segmentation (e.g. BSJ first section 'praṇamya')
-#
-# 	os.chdir('segmenter')
-#  	subprocess.call('python3 -W ignore apply.py ../temp/temp2_resized_for_segmenter.txt ../temp/temp3_segmented.txt', shell='True')
-# 	# '-W ignore' for supppressing warning in Terminal
-# 	os.chdir('..')
-#
-# 	# pick results back up
-# 	f_in = open('temp/temp3_segmented.txt','r')
-# 	postsegmentation_contents = (f_in.read()[:-1]).decode('utf-8')
-# 		# delete spurious final newline added by segmenter
-# 	f_in.close()
-
-# 	# finish clean-up by removing newlines and punctuation, and replacing hyphen with space
-#
-# 	regex_replace = [
-# 	[u'[\n]', ' '],
-# 	[u'[\|/\.,;:—ḷ]', ''], # NB: segmenter turns ? to ḷ via internal transliteration scheme
-# 	['-', ' '], # hyphen, vs em-dash above
-# 	['  ', ' '],
-# 	]
-# 	for r_r in regex_replace:
-# 		postsegmentation_contents = re.sub(r_r[0], r_r[1], postsegmentation_contents)
-
-
-# 	# compare with starting number to get measure of resizing activity
-# 	print("final number of docs:", len(postsegmentation_contents.split('#')))
 
 	# count lines and tokens
 	megaS.word_split_line_count = Spl.line_count_during_split
 	megaS.token_count = Spl.token_count
 
-# 	print("final number of tokens:", num_tokens)
-
 	# reincorporate treated content back into master Section object
-	# megaS.doc_contents = postsegmentation_content.split('#')
 	megaS.doc_contents = postsegmentation_content.split('\n')
 
 	# log
-	logging_path_suffix = '2_word-split_docs/'
+	logging_path_suffix = '3_word-split_docs/'
 	full_logging_f_path = os.path.join(logging_path_prefix, logging_path_suffix, fn)
 	with open(full_logging_f_path ,'w') as f_out:
 		f_out.write('\n'.join(
@@ -307,22 +219,13 @@ for fn in fns:
 			)
 		)
 
-# 	# for debugging or illustration
-# 	f_out = open('temp/temp4_cleaned.txt','w')
-#  	f_out.write('\n'.join(megaS.doc_contents).encode('utf-8'))
-# 	f_out.close()
-#
-# 	# convert doc_identifers to CTS URNS
-# 	# TO-DO...
-#
-# 	# save this file's results to a corresponding two-column tsv file
-# 	f_out = open(os.path.join(output_path, fn),'w')
-# 	for (doc_identifer, doc_content) in zip(megaS.doc_identifers, megaS.doc_contents):
-# 		f_out.write(doc_identifer.encode('utf-8') + '\t' + doc_content.encode('utf-8') + '\n')
-# 	f_out.close()
-#
-# 	# ALTERNATIVELY: could here contribute this file's info to a central CTS file
-# 	# TO-DO...
+ 	# convert doc_identifers to CTS URNS here...
+	# use tsv lookup table ...
+
+	# add all results to CTS file buffer ...
+	# simple join...
+
+	# record individual file stats
 
 	stats_buffer += '\t'.join([
 		fn,
@@ -332,7 +235,7 @@ for fn in fns:
 		str(megaS.token_count)
 		]) + '\n'
 
-# final logging of stats
+# output stats for all files
 full_logging_f_path = os.path.join(logging_path_prefix, 'stats.tsv')
 with open(full_logging_f_path ,'w') as f_out:
 	f_out.write(stats_buffer)
